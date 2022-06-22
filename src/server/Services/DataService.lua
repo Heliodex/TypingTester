5203c97fa5567874f7f071f51a96ef52a751857b
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-06-22 10:58:06 +0100

    Use ProfileService instead of DataStore2

--- src/server/Services/DataService.lua
+++ src/server/Services/DataService.lua
@@ -1,12 +1,32 @@
 local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
-local DataStore2 = require(game:GetService("ServerScriptService").Server.DataStore2)
+local ProfileService = require(game:GetService("ServerScriptService").Server.ProfileService)
+
+local ProfileTemplate = {
+	WordsTyped = 0,
+	Experience = 0,
+	Currency = 0,
+
+	ShopPurchases = {
+		NormalMode = false,
+		HardMode = false,
+		InsaneMode = false,
+	},
+	Settings = {
+		KeySounds = false,
+		BlindMode = false,
+		MemoryMode = false,
+		PlainBG = false,
+	},
+}
 
 local DataService = Knit.CreateService({
 	Name = "DataService",
 })
 
-function DataService.Client:GetData()
+function DataService.Client:LoadData()
 	return "bruh"
 end
+
+function DataService.Client:UpdateData() end
 
 return DataService
